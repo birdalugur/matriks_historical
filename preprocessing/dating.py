bb3b433392cae67e59b1b49a2114a8f6119ac322
birdalugureren <birdalugureren@gmail.com>
2020-04-08 00:30:43 +0300

    edit datingpy

--- preprocessing/dating.py
+++ preprocessing/dating.py
@@ -1,34 +1,63 @@
 #!/usr/bin/env python
 # coding: utf-8
+
+# In[ ]:
+
+
 import pandas as pd
 from multiprocessing import Pool
 
-import_path = '../data/matriksdata.csv'
-export_path = '../data/matriksdata_1.csv'
 
-data = pd.read_csv(import_path)
+# In[ ]:
+
+
+file_path = 'matriksdata.csv'
+
+
+# In[ ]:
+
+
+data = pd.read_csv(file_path)
+
+
+# In[ ]:
+
 
 date = pd.to_datetime(data.date_time)
-nano = pd.to_datetime(data.timestamp_ns, unit='ns')
+nano = pd.to_datetime(data.timestamp_ns,unit='ns')
+
+
+# In[ ]:
+
 
 date_size = len(date)
 
-x = list(range(date_size))
+
+# In[ ]:
+
+
+x=list(range(date_size))
+
+
+# In[ ]:
 
 
 def set_date(i):
-    return pd.Timestamp(month=date[i].month, year=date[i].year, day=date[i].day, hour=nano[i].hour,
-                        minute=nano[i].minute, second=nano[i].second, microsecond=nano[i].microsecond,
-                        nanosecond=nano[i].nanosecond)
+    return pd.Timestamp(month=date[i].month,year=date[i].year,day=date[i].day,hour=nano[i].hour,minute=nano[i].minute,second=nano[i].second,microsecond=nano[i].microsecond,nanosecond=nano[i].nanosecond)
+
+
+# In[ ]:
 
 
 if __name__ == '__main__':
+    
     pool = Pool(processes=8)
-
+    
     results = pool.map(set_date, x)
+    
+    data['date'] = results
 
-    data['date'] = new_date
+    data.drop(['date_time','timestamp_ns'],axis=1,inplace=True)
 
-    data.drop(['date_time', 'timestamp_ns'], axis=1, inplace=True)
+    data.to_csv('matriksdata_1.csv',index=False)
 
-    data.to_csv(export_path, index=False)
